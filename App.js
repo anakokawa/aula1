e79fd2fd525d938a168e78140daf04c2641600e9
anakokawa <133869844+anakokawa@users.noreply.github.com>
2024-02-27 12:22:57 +0000

    letras commit

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>ETEC-Professor Milton Gazzetti</Text>
+      <Text style={styles.italico}>Terceiro Ano</Text>
+      <Text style={styles.textoRiscado}>Texto Riscado</Text>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +20,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textoRiscado: {
+    textDecorationLine: 'line-through',
+    fontSize: 18, // ajuste o tamanho conforme necessário
+  },
+  italico: {
+    fontStyle: 'italic',
+  },
+
 });
